42a067899a1d5e9a804618440dd5e6df5220170d
icelighting <35771160+icelighting@users.noreply.github.com>
2019-02-22 16:35:17 +0800

    Add files via upload

---
+++ 排序与搜索/dfs与bfs.py
@@ -0,0 +1,127 @@
+'''先来创建一个图'''
+
+class Graph(object):
+    def __init__(self, *args, **kwargs):
+        self.node_neightbors = {}
+        self.visited = {}
+
+    def add_nodes(self, nodelist):
+        for node in nodelist:
+            self.add_node(node)
+
+    def add_node(self, node):
+        if not node in self.nodes():
+            self.node_neightbors[node] = []
+
+    def add_edge(self, edge):
+        u, v = edge
+        if (v not in self.node_neightbors[u]) and (u not in self.node_neightbors[v]):
+            self.node_neightbors[u].append(v)
+            if (u != v):
+                self.node_neightbors[v].append(u)
+
+
+    def nodes(self):
+        return self.node_neightbors.keys()
+
+    def deep_first_search(self,root = None):#递归dfs
+        order = []
+        def dfs(node):
+            self.visited[node] = True
+            order.append(node)
+            for n in self.node_neightbors[node]:
+                if not n in self.visited:
+                    dfs(n)
+
+
+        if root:
+            dfs(root)
+
+        for node in self.nodes():##用以处理不连通的结点，需要重新开始，再一次dfs
+            if not node in self.visited:
+                dfs(node)
+
+        self.visited = {}
+        print(order)
+        return order
+
+    def depth_first_search2(self, root =None):#非递归 dfs
+        stack = []
+        order = []
+        def dfs():
+            while stack:
+                node = stack[-1]
+                for n in self.node_neightbors[node]:
+                    if not n in self.visited:
+                        order.append(n)
+                        stack.append(n)
+                        self.visited[n] = True
+                        break
+                else:
+                    stack.pop()
+
+        if root:
+            stack.append(root)
+            order.append(root)
+            self.visited[root] = True
+            dfs()
+
+        for node in self.nodes():
+            if node not in self.visited:
+                stack.append(node)
+                order.append(node)
+                self.visited[node] =True
+                dfs()
+
+        self.visited= {}
+        print(order)
+        return order
+
+    def breadth_first_search(self,root=None):
+         queue = []
+         order = []
+         def bfs():
+             while len(queue)>0:
+                 node = queue.pop(0)
+                 self.visited[node] = True
+                 for n in self.node_neightbors[node]:
+                     if (not n in self.visited) and (not n in queue):
+                         queue.append(n)
+                         order.append(n)
+         if root:
+             queue.append(root)
+             order.append(root)
+             bfs()
+         for node in self.nodes():
+             if not node in self.visited:
+                 queue.append(node)
+                 order.append(node)
+                 bfs()
+
+         self.visited = {}
+         print(order)
+         return order
+
+
+if __name__ == '__main__':
+    ## 关于递归的dfs
+    g = Graph()
+    g.add_nodes([i + 1 for i in range(8)])
+    g.add_edge((1, 2))
+    g.add_edge((1, 3))
+    g.add_edge((2, 4))
+    g.add_edge((2, 5))
+    g.add_edge((4, 8))
+    g.add_edge((5, 8))
+    g.add_edge((3, 6))
+    g.add_edge((3, 7))
+    g.add_edge((6, 7))
+    print("nodes:", g.nodes())
+    print("BFS：")
+    order = g.breadth_first_search(1)
+    # self.visited 在经历了一次bfs之后已经有了值，如果dfs直接进行，就会发生只输出结点1的情况
+    print("递归DFS：" )
+    order = g.deep_first_search(1)
+    print ("非递归DFS" )
+    order = g.depth_first_search2(1)
+
